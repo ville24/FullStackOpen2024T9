57b6d232e211a5ff6385833f051f8d8f19be3308
ville24 <73233385+ville24@users.noreply.github.com>
2024-07-31 13:05:04 +0000

    T9.3

--- BMI/bmiCalculator.ts
+++ BMI/bmiCalculator.ts
@@ -1,6 +1,25 @@
+interface BmiValues {
+    value1: number;
+    value2: number;
+}
+  
+const parseArguments = (args: string[]): BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        if (Number(args[2]) === 0) throw new Error('Division by zero')
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+  
 const calculateBmi = (height: number, weight: number): string => {
     const bmi: number = weight / Math.pow(height/100, 2);
-    console.log(bmi)
 
     switch (true) {
         case bmi < 16:
@@ -34,10 +53,15 @@ const calculateBmi = (height: number, weight: number): string => {
         default:
             return 'Obese (Class III) ';
     }
-    
 }
 
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
-
-console.log(calculateBmi(height, weight));
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
---
+++ BMI/exerciseCalculator.ts
@@ -0,0 +1,72 @@
+interface ExerciseValues {
+    target: number,
+    exerciseData: number[]
+}
+
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const parseExerciseArguments = (args: string[]): ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+    let argsCount = 3;
+    let exerciseData: number[] = [];
+    while (args[argsCount]) {
+        exerciseData.push(Number(args[argsCount]));
+        argsCount++;
+    }
+    if (isNaN(Number(args[2]))) throw new Error('Provided values were not numbers!');
+    if (Number(args[2]) === 0) throw new Error('Division by zero');
+
+    exerciseData.map(day => {
+        if (isNaN(Number(day))) throw new Error('Provided values were not numbers!');
+    })
+    return {
+        target: Number(args[2]),
+        exerciseData: exerciseData
+    }
+}
+
+const calculateExercises = (target: number, data: number[]): Result => {
+    const ratingRatio: number = Math.round((data.reduce((a, b) => a + b, 0) / data.length) / target);
+    let rating: number;
+    switch (true) {
+        case ratingRatio < 0.5:
+            rating = 1;
+            break;
+        case ratingRatio < 1.5:
+            rating = 2;
+            break;
+        default:
+            rating = 3;
+            break;
+    }
+    const ratingDescription: string[] = ['bad', 'not too bad but could be better', 'good'];
+    return {
+        periodLength: data.length,
+        trainingDays: data.filter(day => day != 0).length,
+        success: (data.reduce((a, b) => a + b, 0) / data.length) > target,
+        rating: rating,
+        ratingDescription: ratingDescription[rating-1],
+        target: target,
+        average: data.reduce((a, b) => a + b, 0) / data.length
+    }
+}
+
+try {
+    const { target, exerciseData } = parseExerciseArguments(process.argv);
+    console.log(calculateExercises(target, exerciseData));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
